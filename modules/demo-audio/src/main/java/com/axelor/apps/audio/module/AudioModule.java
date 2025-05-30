1a2c43c453f21a171e1681cb59c54d193490082c
a1tbaev <aitbaevadil17@gmail.com>
2025-05-30 12:04:25 +0600

    deleted event log

--- modules/demo-audio/src/main/java/com/axelor/apps/audio/module/AudioModule.java
+++ modules/demo-audio/src/main/java/com/axelor/apps/audio/module/AudioModule.java
@@ -11,10 +11,7 @@ import com.axelor.apps.audio.service.impl.SoundServiceImpl;
 import com.axelor.apps.audio.service.impl.SoundTaskServiceImpl;
 import com.axelor.apps.audio.websocket.SessionStorage;
 import com.axelor.apps.audio.websocket.WebSocketServer;
-import com.axelor.apps.audio.websocket.channesl.EvenLogChanel;
-import com.axelor.web.socket.Channel;
 import com.axelor.web.socket.WebSocketEndpoint;
-import com.google.inject.multibindings.Multibinder;
 
 public class AudioModule extends AxelorModule {
 
@@ -26,8 +23,6 @@ public class AudioModule extends AxelorModule {
         bind(SoundService.class).to(SoundServiceImpl.class);
         bind(SessionStorage.class).asEagerSingleton();
 
-        Multibinder<Channel> multibinder = Multibinder.newSetBinder(this.binder(), Channel.class);
-        multibinder.addBinding().to(EvenLogChanel.class);
         this.bind(WebSocketEndpoint.class).asEagerSingleton();
         bind(WebSocketServer.class).asEagerSingleton();
     }
