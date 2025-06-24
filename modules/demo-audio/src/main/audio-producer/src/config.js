5eb01e1a75b6e89a687880f8d30de9ae7559834b
a1tbaev <aitbaevadil17@gmail.com>
2025-06-24 17:22:46 +0600

    config.js to ignore

--- modules/demo-audio/src/main/audio-producer/src/config.js
+++
@@ -1,9 +0,0 @@
-export const WS_PROXY_PORT = 3001;
-
-export const AXELOR_HTTP_BASE_URL = 'http://192.168.11.135:8080/audio';
-export const AXELOR_WS_BASE_URL = 'ws://192.168.11.135:8080/audio';
-
-export const CUSTOMS_OFFICE_BASE_URL = 'http://192.168.11.135:8080/audio/ws/public/customs-office';
-
-export const AXELOR_USER_NAME = 'admin';
-export const AXELOR_PASSWORD = 'IGj47O6D2&Om';
